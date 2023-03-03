274d8c158ef583b20e3f07ecc62318e08553f60d
Bondarni <118389811+Bondarni@users.noreply.github.com>
2023-03-03 13:15:43 -0500

    installed front end dependencies and reformatted index.js

---
+++ client/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import App from './App'
+import { BrowserRouter } from 'react-router-dom'
+import { createRoot } from 'react-dom/client'
+
+const root = createRoot(document.getElementById('root'))
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+)
