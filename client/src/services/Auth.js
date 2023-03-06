861c7c6587d78e7d1a891a6d647ddb73aa12bfbe
Bondarni <118389811+Bondarni@users.noreply.github.com>
2023-03-06 17:50:52 -0500

    fixed registration issue

--- client/src/services/Auth.js
+++ client/src/services/Auth.js
@@ -4,6 +4,7 @@ export const SignInPatient = async (data) => {
   try {
     const res = await Patient.post('api/patients/login', data)
     // Set the current signed in users token to localStorage
+    localStorage.setItem('token', res.data.token)
     return res.data.patient
   } catch (error) {
     throw error
@@ -13,7 +14,7 @@ export const SignInPatient = async (data) => {
 export const RegisterPatient = async (data) => {
   try {
     const res = await Patient.post('api/patients/register', data)
-    return res.data.patient
+    return res.data
   } catch (error) {
     throw error
   }
