c88fe268088cef96522a02ee10dc89107177c51b
Joshua Jumelet <joshuajumelet@gmail.com>
2023-03-08 06:20:55 -0800

    updated axios calls for Doctors to App.js

--- client/src/pages/Doctors.js
+++ client/src/pages/Doctors.js
@@ -5,19 +5,6 @@ import { BASE_URL } from '../services/api'
 import DocCard from '../components/DocCard'
 
 const Doctors = () => {
-  const [allDoctors, setAllDoctors] = useState({})
-
-  const GetDoctors = async () => {
-    const res = await axios.get(`${BASE_URL}/api/doctors`)
-    console.log(res.data)
-    setAllDoctors(res.data)
-  }
-
-  useEffect(() => {
-    GetDoctors()
-  }, [])
-  console.log(allDoctors)
-
   return (
     <div className="doctors">
       <div className="search">Search Functionality goes here</div>
