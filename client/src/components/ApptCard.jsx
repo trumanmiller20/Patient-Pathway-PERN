d95913a92cdbdcf9b261ab907c1e8d01234d8986
trumanmiller <trumanmiller20@gmail.com>
2023-03-09 14:39:41 -0700

    start EditAppt to incorporate update capability

--- client/src/components/ApptCard.jsx
+++ client/src/components/ApptCard.jsx
@@ -1,15 +1,13 @@
 import axios from 'axios'
 import { useEffect, useState } from 'react'
 import { BASE_URL } from '../services/api'
+import { useNavigate } from 'react-router-dom'
+import EditAppt from './EditAppt'
 
 const ApptCard = ({ patient, allAppointments, allDoctors, allPatients }) => {
+  let navigate = useNavigate()
   const [patientAppts, setPatientAppts] = useState({})
-  const [updated, setUpdated] = useState(false)
-  const [edit, setEdit] = useState({
-    visit_reason: '',
-    date: '',
-    time: ''
-  })
+
 
   const getAppointmentsByPatient = async () => {
     const res = await axios.get(
@@ -36,32 +34,7 @@ const ApptCard = ({ patient, allAppointments, allDoctors, allPatients }) => {
     }
   }, [patient])
 
-  const handleSubmit = async (e) => {
-    e.preventDefault()
-    const token = localStorage.getItem('token')
-    const config = {
-      headers: {
-        Authorization: `Bearer ${token}`
-      }
-    }
-    await axios.put(`${BASE_URL}/api/appointments${e.target.id}`, config, {
-      visit_reason: edit.visit_reason,
-      date: edit.date,
-      time: edit.time
-    })
-    setEdit(...edit)
-    setPatientAppts(...patientAppts)
-  }
-
-  const update = () => {
-    setUpdated(true)
-  }
-
-  const handleChange = (e) => {
-    setEdit({ ...edit, [e.target.id]: e.target.value })
-  }
-
-  return (
+return (
     <div>
       {allAppointments?.map((appt, index) => (
         <div key={index}>
@@ -78,36 +51,7 @@ const ApptCard = ({ patient, allAppointments, allDoctors, allPatients }) => {
           >
             Cancel Appointment
           </button>
-          <div>
-            <button onClick={update}>Update Appointment Details</button>
-            {updated && (
-              <form onSubmit={handleSubmit}>
-                <label>Reason For Visit:</label>
-                <input
-                  type="visit_reason"
-                  name="visit_reason"
-                  value={edit.visit_reason}
-                  onChange={handleChange}
-            ></input>
-                <label>Preferred Date:</label>
-                <input
-                  type="date"
-                  name="date"
-                  value={edit.date}
-                  onChange={handleChange}
-                ></input>
-                <label>Preferred Time:</label>
-                <input
-                  type="time"
-                  name="time"
-                  value={edit.time}
-                  onChange={handleChange}
-                ></input>
-                <button id={patientAppts[index]?.id}
-            onClick={(e) => handleSubmit(e)} type="submit">Update</button>
-              </form>
-            )}
-          </div>
+            <button onClick={() => navigate(`/update/${appt.id}`)}>Update Appointment Details</button>
         </div>
       ))}
     </div>
